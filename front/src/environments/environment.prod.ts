52c6babd67b6b51dcf9e01c05499b155a608569e
Sebastian21334 <sebastiangonzalez100106@gmail.com>
2026-09-13 02:44:18 -0300

    Url de back actualizada

--- front/src/environments/environment.prod.ts
+++ front/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@
 // (App Service / Container Apps, etc.). Se resuelve vía fileReplacements en angular.json.
 export const environment = {
   production: true,
-  apiUrl: 'https://TU-BACKEND-EN-AZURE.azurewebsites.net',
+  apiUrl: 'https://seminario-integrador.onrender.com',
 };
